a04d583df271e025ca951925a11b0798b486cad3
Ignas Kadisa <kadisaignas@gmail.com>
2025-06-03 09:12:36 +0200

    converted from console to raylib. basic drawtext gui displaying rules, slots, credit value etc.

--- Project2.cpp
+++ Project2.cpp
@@ -12,15 +12,15 @@ std::string message = "";
 Sound credit_sound;
 Sound jackpot;
 
-void runSlot(int& credits) {
-
+void runSlot(int& credits) 
+{
     slotValues[0] = rand() % 22;
     slotValues[1] = rand() % 22;
     slotValues[2] = rand() % 22;
 
     message = "";
 
-    if (slotValues[0] == slotValues[1] && slotValues[1] == slotValues[2]) {
+ /*   if (slotValues[0] == slotValues[1] && slotValues[1] == slotValues[2]) {
         credits += 1000;
         message = "Jackpot! +1000 credits";
         PlaySound(jackpot);
@@ -31,18 +31,14 @@ void runSlot(int& credits) {
             credits += 10;
             message = "21! +10 credits";
             PlaySound(credit_sound);
-            
         }
         else if (slotValues[i] == 7) {
             credits += 75;
             message = "7! +75 credits";
             PlaySound(credit_sound);
         }
-    }
+    }*/
 }
-
-
-
 
 int main() 
 {
@@ -51,20 +47,25 @@ int main()
     credit_sound = LoadSound("credits.mp3");
     jackpot = LoadSound("jackpot.mp3");
     SetTargetFPS(60);
-    srand(static_cast<unsigned int>(time(0)));
+    srand(static_cast<unsigned int>(time(0))); //starting value 0, this seeds rand and helps generate random numbers. supposedly not efficient but i do not care right now
 
-    while (!WindowShouldClose()) 
+    while (!WindowShouldClose()) //check if KEY_ESCAPE pressed, windows close icon clicked
     {
-    
-        if (IsKeyPressed(KEY_SPACE) && credits >= 5) {
-            credits -= 5;
-            runSlot(credits);
-        }
+
 
         BeginDrawing();
         ClearBackground(BLACK);
 
         DrawText("Welcome to the slots!", 200, 20, 30, RED);
+
+        if (IsKeyPressed(KEY_SPACE) && credits >= 5) {
+            credits -= 5;
+            runSlot(credits);
+        }
+        if (IsKeyPressed(KEY_SPACE) && credits < 5) {
+            BeginDrawing();
+            DrawText("No credits left.", 20, 335, 24, RED); //it just flashes up on screen for a second. i think it's a Begin+EndDrawing() issue??
+        }
 
         DrawText("Rules:", 20, 60, 20, WHITE);
         DrawText("- Hit 21 = +10 credits", 20, 90, 18, WHITE);
